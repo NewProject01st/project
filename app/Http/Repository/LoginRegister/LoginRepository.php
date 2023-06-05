bc8cbf2af9030df9baf3e42e2b3a4f64682fe563
Satish <satishaurange@sumagoinfotech.com>
2023-06-05 14:33:37 +0530

    changes for login

--- app/Http/Repository/LoginRegister/LoginRepository.php
+++ app/Http/Repository/LoginRegister/LoginRepository.php
@@ -22,22 +22,30 @@ class LoginRepository
     public function checkLogin($request) {
         // get school description
         $data = [];
-        $data['user_details'] = User::where('u_email','=',$request['email'])->select('*')->first();
-        $data['user_permission'] = RolesPermissions::join('permissions', function($join) {
-                                        $join->on('roles_permissions.permission_id', '=', 'permissions.id')
-                                        ->where('permissions.is_active','=',true);
-                                    })
-                                    ->where('roles_permissions.is_active','=',true)
-                                    ->where('user_id','=',$data['user_details']->id)
-                                    ->select(                                    "roles_permissions.per_add",
-                                            "roles_permissions.per_edit",
-                                            "roles_permissions.per_update",
-                                            "roles_permissions.per_delete",
-                                            "permissions.route_name",
-                                            "permissions.permission_name",
-                                            "permissions.url")
-                                    ->get()
-                                    ->toArray();
+        $data['user_details'] = User::where( [
+                                        'u_email' => $request['email'],
+                                        'is_active' =>true
+                                        ])
+                                        ->select('*')
+                                        ->first();
+        if($data['user_details']) {
+            $data['user_permission'] = RolesPermissions::join('permissions', function($join) {
+                                            $join->on('roles_permissions.permission_id', '=', 'permissions.id')
+                                            ->where('permissions.is_active','=',true);
+                                        })
+                                        ->where('roles_permissions.is_active','=',true)
+                                        ->where('user_id','=',$data['user_details']->id)
+                                        ->select(
+                                                "roles_permissions.per_add",
+                                                "roles_permissions.per_edit",
+                                                "roles_permissions.per_update",
+                                                "roles_permissions.per_delete",
+                                                "permissions.route_name",
+                                                "permissions.permission_name",
+                                                "permissions.url")
+                                        ->get()
+                                        ->toArray();
+        }
         return $data;
     }
 }
