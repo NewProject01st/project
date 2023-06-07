2b3334c312ed4755cb62656fd2c38a6df5fc016a
Savita444 <savita.hajare@sumagoinfotech.com>
2023-06-07 12:39:06 +0530

    change marquee

--- resources/views/admin/pages/home/Marquee/add-marquee.blade.php
+++ resources/views/admin/pages/home/Marquee/add-marquee.blade.php
@@ -71,37 +71,5 @@
             </div>
         </div>
     </div>
-    {{-- <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-        <script>
-        $(document).ready(function() {
-            $("#regForm").validate({
-                rules: {
-                  english_title: {
-                        required: true,
-                        charactersOnly: true // Use custom validation method
-                    },
-                    marathi_title: {
-                        required: true,
-                        // maxlength: 20,
-                        charactersOnly: true // Use custom validation method
-                    },
-                   
-                  },
-                messages: {
-                  english_title: {
-                        required: "Title name is required",
-                        charactersOnly: "Only characters are allowed." // Custom error message for custom validation method
-                    },
-                    marathi_title: {
-                      required: "Title name is required",
-                      charactersOnly: "Only characters are allowed." // Custom error message for custom validation method
-                    },
-                    
-                   
-                }
-            });
-        });
-    </script> --}}
-
-
+    
     @endsection
