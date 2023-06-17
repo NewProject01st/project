475f9371d8ba844898e0174b6c5518737746c344
Satish <satishaurange@sumagoinfotech.com>
2023-06-17 15:29:44 +0530

    changes for css file added

--- resources/views/website/layout/header.blade.php
+++ resources/views/website/layout/header.blade.php
@@ -56,7 +56,7 @@
     <!-- <link href="{{ asset('website_files/assets/font-awesome.min') }}" type="text/css" rel="stylesheet"> -->
     <!-- ss -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-
+    <link href="{{ asset('website_files/assets/css/all.css') }}" type="text/css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
         integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
