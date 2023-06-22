12ebd49a65c6439952dcde343272a83a60528419
Savita444 <savita.hajare@sumagoinfotech.com>
2023-06-22 11:39:04 +0530

    changes

--- resources/views/admin/pages/citizen-action/incident-type/add-incident-type.blade.php
+++ resources/views/admin/pages/citizen-action/incident-type/add-incident-type.blade.php
@@ -46,6 +46,7 @@
                                             @endif
                                         </div>
                                     </div>
+                                    
                                     <div class="col-md-12 col-sm-12 text-center">
                                         <button type="submit" class="btn btn-success">Save &amp; Submit</button>
                                         {{-- <button type="reset" class="btn btn-danger">Cancel</button> --}}
