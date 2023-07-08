643ebd1d38d1cc0e666ddac2decf59c82d523371
Savita444 <savita.hajare@sumagoinfotech.com>
2023-07-08 14:38:15 +0530

    changes

--- resources/views/website/pages/citizen-action/volunteer-citizen-support-web.blade.php
+++ resources/views/website/pages/citizen-action/volunteer-citizen-support-web.blade.php
@@ -300,13 +300,6 @@
                         </form>
                     </div>
                 </div>
-                {{-- <div class="row">
-                     <div class="col-md-12">
-                        <div class="map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3867187.666169696!2d76.76983739999999!3d18.81817715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1683036229388!5m2!1sen!2sin"></iframe>
-                        </div>
-                     </div>
-                  </div> --}}
             </div>
         </div>
         <!-- Google Map with Contact Form End -->
