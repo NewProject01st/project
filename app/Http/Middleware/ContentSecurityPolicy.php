6bd5d096968737e19225313c8e4f9b6fc52a1e35
Satish <satishaurange@sumagoinfotech.com>
2023-07-26 14:27:38 +0530

    changes for ContentSecurityPolicy file changes new

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -9,8 +9,8 @@ class ContentSecurityPolicy
     {
         $response = $next($request);
         $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-eval' https://www.google-analytics.com https://code.jquery.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' https://www.w3.org https://liveserverdm.s3.ap-south-1.amazonaws.com https://www.google-analytics.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://www.google-analytics.com; frame-src 'self';");
-        $response->headers->set('X-Content-Security-Policy', "script-src 'self' 'unsafe-eval' https://www.google-analytics.com https://code.jquery.com 'nonce-$nonce';"); // For older browsers that don't support CSP Level 2
-        $response->headers->set('X-WebKit-CSP', "script-src 'self' 'unsafe-eval' https://www.google-analytics.com https://code.jquery.com 'nonce-$nonce';"); // For older versions of Chrome/Safari
+        $response->headers->set('X-Content-Security-Policy', "script-src 'self' 'unsafe-eval' https://www.google-analytics.com https://code.jquery.com;"); // For older browsers that don't support CSP Level 2
+        $response->headers->set('X-WebKit-CSP', "script-src 'self' 'unsafe-eval' https://www.google-analytics.com https://code.jquery.com ;"); // For older versions of Chrome/Safari
         
         return $response;
     }
