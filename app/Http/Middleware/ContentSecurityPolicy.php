fb437a451c67fcad3b400048e053d1365e263594
Satish <satishaurange@sumagoinfotech.com>
2023-07-26 13:57:36 +0530

    changes for ContentSecurityPolicy middleware added

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -9,7 +9,15 @@ class ContentSecurityPolicy
     {
         $response = $next($request);
         
-        $response->headers->set('Content-Security-Policy',"default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self'; font-src 'self';");
+        $response->headers->set('Content-Security-Policy', "
+            default-src 'self';
+            <!-- script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google-analytics.com;
+            style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+            img-src 'self' https://www.google-analytics.com;
+            font-src 'self' https://fonts.gstatic.com;
+            connect-src 'self' https://www.google-analytics.com;
+            frame-src 'self'; -->
+        ");
 
         return $response;
     }
