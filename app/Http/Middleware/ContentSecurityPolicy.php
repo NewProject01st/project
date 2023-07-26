b64e1d9739ce76afe53115b2f02f138292906dfb
Satish <satishaurange@sumagoinfotech.com>
2023-07-26 14:03:12 +0530

    changes for ContentSecurityPolicy file changes new

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -8,17 +8,7 @@ class ContentSecurityPolicy
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
-        $response->headers->set('Content-Security-Policy', "
-        default-src 'self';
-        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google-analytics.com;
-        style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-        img-src 'self' https://www.google-analytics.com;
-        font-src 'self' https://fonts.gstatic.com;
-        connect-src 'self' https://www.google-analytics.com;
-        frame-src 'self';
-    ");
-        
-
+        $response->headers->set('Content-Security-Policy', "default-src 'self';script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google-analytics.com;style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;img-src 'self' https://www.google-analytics.com;font-src 'self' https://fonts.gstatic.com;connect-src 'self' https://www.google-analytics.com;frame-src 'self';");
         return $response;
     }
 }
