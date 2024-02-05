e6e8ee370cee16531b398ffb8cfeb263b6187aa1
Savita444 <savita.hajare@sumagoinfotech.com>
2024-02-05 14:27:44 +0530

    changes session file

--- resources/views/website/pages/home.blade.php
+++ resources/views/website/pages/home.blade.php
@@ -82,8 +82,8 @@
                                 <div class="carousel-caption">
                                     <h1 data-aos="fade-right" data-aos-anchor="#example-anchor"
                                     data-aos-offset="500" data-aos-duration="2000"><?php echo $slider['marathi_title']; ?></h1>
-                                    <p  data-aos="fade-left" data-aos-anchor="#example-anchor"
-                                    data-aos-offset="500" data-aos-duration="2000"><?php echo $slider['marathi_description']; ?> </p>
+                                    <h6  data-aos="fade-left" data-aos-anchor="#example-anchor"
+                                    data-aos-offset="500" data-aos-duration="2000"><?php echo $slider['marathi_description']; ?> </h6>
                                     <div class="slide-content-box pt-3" data-aos="fade-right" data-aos-anchor="#example-anchor"
                                     data-aos-offset="500" data-aos-duration="2000"> <a href="{{ $slider['url'] }}" class="con"
                                             target="_blank">
@@ -102,15 +102,8 @@
                                 <div class="carousel-caption">
                                     <h1 data-aos="fade-right" data-aos-anchor="#example-anchor"
                                     data-aos-offset="500" data-aos-duration="2000"><?php echo $slider['english_title']; ?></h1>
-
-
                                     <h6 data-aos="fade-left" data-aos-anchor="#example-anchor"
                                     data-aos-offset="500" data-aos-duration="2000"><?php echo $slider['english_description']; ?> </h6>
-
-                                    {{-- <h6 class="dis-display new-dis-size" data-aos="fade-left" data-aos-anchor="#example-anchor" data-aos-offset="500"
-                                    data-aos-duration="3000"><?php //echo $slider['english_description']; ?> --}}
-
-
                                     <div class="slide-content-box pt-3" data-aos="fade-right" data-aos-anchor="#example-anchor"
                                     data-aos-offset="500" data-aos-duration="2000"> <a href="{{ $slider['url'] }}" class="con"
                                             target="_blank">
