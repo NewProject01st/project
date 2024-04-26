cc1401869727bb763de3cb4e47a2b4cabfba5acd
Savita444 <savita.hajare@sumagoinfotech.com>
2024-04-26 12:39:31 +0530

    change xml file and forcast button colour

--- resources/views/website/pages/disaster-forecast-web.blade.php
+++ resources/views/website/pages/disaster-forecast-web.blade.php
@@ -6,27 +6,11 @@
         <div class="container">
             <h2>
                 @if (session('language') == 'mar')
-                    {{ Config::get('marathi.NEWS_EVENTS.NEWS_EVENTS_HEADING') }}
-                @else
-                    {{ Config::get('english.NEWS_EVENTS.NEWS_EVENTS_HEADING') }}
-                @endif
-            </h2>
-            <ul>
-                <li> <a href="{{ route('disaster-management-news') }}">
-                        @if (session('language') == 'mar')
-                            {{ Config::get('marathi.NEWS_EVENTS.NEWS_EVENTS_MAIN_LINK') }}
-                        @else
-                            {{ Config::get('english.NEWS_EVENTS.NEWS_EVENTS_MAIN_LINK') }}
-                        @endif
-                    </a> </li>
-                <li>
-                    @if (session('language') == 'mar')
                     {{ Config::get('marathi.HOME_PAGE.DISASTER_FORCAST') }}
                 @else
                     {{ Config::get('english.HOME_PAGE.DISASTER_FORCAST') }}
                 @endif
-                </li>
-            </ul>
+            </h2>
         </div>
     </section>
     <!--Subheader End-->
@@ -39,12 +23,11 @@
                 <div class="container ">
                     <div class="title-style-3 deprt-txt">
                         <h3>
-                            Disaster Forecaste
-                            {{-- @if (session('language') == 'mar')
-                                {{ Config::get('marathi.NEWS_EVENTS.DISASTER_MANAGEMENT_NEWS') }}
+                            @if (session('language') == 'mar')
+                                {{ Config::get('marathi.HOME_PAGE.DISASTER_FORCAST') }}
                             @else
-                                {{ Config::get('english.NEWS_EVENTS.DISASTER_MANAGEMENT_NEWS') }}
-                            @endif --}}
+                                {{ Config::get('english.HOME_PAGE.DISASTER_FORCAST') }}
+                            @endif
                         </h3>
 
                     </div>
@@ -56,32 +39,32 @@
                                     <div class="news-box">
                                         <div class="new-thumb">
                                             {{-- <span class="cat c1">Fire</span> --}}
-                                            
+
                                         </div>
                                         <div class="new-txt">
-                                            
+
                                             <h6 class="card_title_main"><a href="#"><?php echo $item['marathi_title']; ?></a>
                                             </h6>
                                             <p class="card_title"> <?php echo $item['marathi_description']; ?></p>
                                         </div>
-                                        <div class="news-box-f"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  @if (session('language') == 'mar')
-                                            {{ Config::get('marathi.HOME_PAGE.READ_MORE') }}
-                                        @else
-                                            {{ Config::get('english.HOME_PAGE.READ_MORE') }}
-                                        @endif <a
-                                            data-id="{{ $item['id'] }}" class="disaster-show-btn"><i
-                                                class="fa fa-arrow-right"></i></a>
-                                    </div>
-                                        {{-- <div class="news-box-f cursor-pointer">
-                                            <span class="pl-3">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                                @if (session('language') == 'mar')
-                                                    {{ Config::get('marathi.HOME_PAGE.READ_MORE') }}
-                                                @else
-                                                    {{ Config::get('english.HOME_PAGE.READ_MORE') }}
-                                                @endif
-                                            </span> <a data-id="{{ $item['id'] }}" class="show-btn"><i
-                                                    class="fas fa-arrow-right"></i></a>
-                                        </div> --}}
+
+                                        <div class="text-center readmorebtn">
+                                            <a data-id="{{ $item['id'] }}" class="disaster-show-btn cursor-pointer">
+                                                <div class="cursor-pointer">
+
+                                                    <span class="pl-3">
+
+                                                        @if (session('language') == 'mar')
+                                                            {{ Config::get('marathi.HOME_PAGE.READ_MORE') }}
+                                                        @else
+                                                            {{ Config::get('english.HOME_PAGE.READ_MORE') }}
+                                                        @endif
+
+                                                    </span>
+
+                                                </div>
+                                            </a>
+                                        </div>
                                     </div>
                                 </div>
                             @else
@@ -89,35 +72,47 @@
                                     <div class="news-box">
                                         <div class="new-thumb">
                                             {{-- <span class="cat c1">Fire</span> --}}
-                                            
+
                                         </div>
                                         <div class="new-txt">
                                             <h6 class="card_title_main"><a href="#"><?php echo $item['english_title']; ?>...</a>
                                             </h6>
                                             <p class="card_title"> <?php echo $item['english_description']; ?>...</p>
                                         </div>
-                                        <div class="news-box-f"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Read more <a
-                                                data-id="{{ $item['id'] }}" class="disaster-show-btn"><i
-                                                    class="fa fa-arrow-right"></i></a>
+
+                                        <div class="text-center readmorebtn">
+                                            <a data-id="{{ $item['id'] }}" class="disaster-show-btn cursor-pointer">
+                                                <div class="cursor-pointer">
+
+                                                    <span class="pl-3">
+
+                                                        @if (session('language') == 'mar')
+                                                            {{ Config::get('marathi.HOME_PAGE.READ_MORE') }}
+                                                        @else
+                                                            {{ Config::get('english.HOME_PAGE.READ_MORE') }}
+                                                        @endif
+
+                                                    </span>
+
+                                                </div>
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
                             @endif
                         @empty
                             <h4>
-                                Disaster Forecaste
-                                {{-- @if (session('language') == 'mar')
-                                    {{ Config::get('marathi.NEWS_EVENTS.NO_DATA_FOUND_DISASTER_MANAGEMENT_NEWS') }}
+                                @if (session('language') == 'mar')
+                                    {{ Config::get('marathi.HOME_PAGE.DISASTER_FORCAST') }}
                                 @else
-                                    {{ Config::get('english.NEWS_EVENTS.NO_DATA_FOUND_DISASTER_MANAGEMENT_NEWS') }}
-                                @endif --}}
+                                    {{ Config::get('english.HOME_PAGE.DISASTER_FORCAST') }}
+                                @endif
                             </h4>
                         @endforelse
-                          <form method="POST" action="{{ url('/particular-disaster_forecast') }}"
-                                id="disastershowform">
-                                @csrf
-                                <input type="hidden" name="disaster_show_id" id="disaster_show_id" value="">
-                            </form> 
+                        <form method="POST" action="{{ url('/particular-disaster_forecast') }}" id="disastershowform">
+                            @csrf
+                            <input type="hidden" name="disaster_show_id" id="disaster_show_id" value="">
+                        </form>
 
                         <!--News Box End-->
                     </div>
